f6fcefe63276870513ffe1f5c8c1c0b2cbe9e3d0
Cesar Castro Rubio <ccastro@beboplogin3.lcrc.anl.gov>
2025-08-27 14:09:32 -0500

    added dimerized smiles

--- aggregate_data.sh
+++ aggregate_data.sh
@@ -2,8 +2,9 @@
 rm directory_key_smiles.dat
 for dir in $(ls -d ../l1* | cut -d"/" -f2 | sort -V) $(ls -d ../l2* | cut -d"/" -f2 | sort -V) $(ls -d ../l3* | cut -d"/" -f2 | sort -V) 
 do
-	smiles=$(grep "smiles" ../$dir/nematic_dft.py | head -1 | cut -d"\"" -f2)
-	python polymerize_dp.py "$smiles" 2 | awk -v dir=$dir '{print dir,$1}' >> directory_key_smiles_dimerized.dat
+	grep "smiles" ../$dir/nematic_dft.py | head -1 | cut -d"\"" -f2 | awk -v dir=$dir '{print dir,$1}' >> directory_key_smiles.dat
+	#smiles=$(grep "smiles" ../$dir/nematic_dft.py | head -1 | cut -d"\"" -f2)
+	#python polymerize_dp.py "$smiles" 2 | awk -v dir=$dir '{print dir,$1}' >> directory_key_smiles_dimerized.dat
 	#mkdir $dir
 	#cp ../$dir/minim*xyz $dir/
 done
